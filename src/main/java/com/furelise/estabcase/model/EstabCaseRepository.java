e23184d36858c31c5d677fc138030b08c4bc3b40
Jackson <403490484@gms.tku.edu.tw>
2023-12-23 10:43:15 +0800

    新增 EstabCaseRepository 方法

--- src/main/java/com/furelise/estabcase/model/EstabCaseRepository.java
+++ src/main/java/com/furelise/estabcase/model/EstabCaseRepository.java
@@ -4,6 +4,9 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.furelise.product.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,11 +19,13 @@ import javax.transaction.Transactional;
 @Repository
 public interface EstabCaseRepository extends JpaRepository<EstabCase, Integer> {
 
-//	@Query("SELECT * FROM estabCase WHERE planOrdID = ?1 ORDER BY estabCaseDate DESC;")
+	//	@Query("SELECT * FROM estabCase WHERE planOrdID = ?1 ORDER BY estabCaseDate DESC;")
 	List<EstabCase> findByPlanOrdIDOrderByEstabCaseDateDesc(Integer planOrdID);
 
 	Integer countByPlanOrdIDAndEstabCaseDateGreaterThanEqual(Integer planOrdID, Date estabCaseDate);
+
 	List<EstabCase> findByPlanOrdIDAndEstabCaseDateGreaterThanEqual(Integer planOrdID, Date estabCaseDate);
+
 	@Modifying
 	@Query("UPDATE EstabCase e SET e.estabCaseStatus = 3 WHERE e.planOrdID =:planOrdID AND e.estabCaseDate >=:estabCaseDate")
 	void updateEstabCaseStatus(@Param("planOrdID") Integer planOrdID, @Param("estabCaseDate") Date estabCaseDate);
@@ -29,5 +34,8 @@ public interface EstabCaseRepository extends JpaRepository<EstabCase, Integer> {
 			Integer empID,
 			Integer estabCaseStatus
 	);
-	List<EstabCase> findByEmpID(Integer empID);
+
+	EstabCase findByEmpID(Integer empID);
+
+	Page<EstabCase> findAllByTakeStatus(boolean takeStatus, Pageable pageable);
 }
