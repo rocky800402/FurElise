552a8e1620a6e123a824b96b2889368285973144
Jackson <403490484@gms.tku.edu.tw>
2023-12-23 10:54:53 +0800

    修正 EstabCaseRepository 衝突

--- src/main/java/com/furelise/estabcase/model/EstabCaseRepository.java
+++ src/main/java/com/furelise/estabcase/model/EstabCaseRepository.java
@@ -19,23 +19,23 @@ import javax.transaction.Transactional;
 @Repository
 public interface EstabCaseRepository extends JpaRepository<EstabCase, Integer> {
 
-	//	@Query("SELECT * FROM estabCase WHERE planOrdID = ?1 ORDER BY estabCaseDate DESC;")
-	List<EstabCase> findByPlanOrdIDOrderByEstabCaseDateDesc(Integer planOrdID);
+    //	@Query("SELECT * FROM estabCase WHERE planOrdID = ?1 ORDER BY estabCaseDate DESC;")
+    List<EstabCase> findByPlanOrdIDOrderByEstabCaseDateDesc(Integer planOrdID);
 
-	Integer countByPlanOrdIDAndEstabCaseDateGreaterThanEqual(Integer planOrdID, Date estabCaseDate);
+    Integer countByPlanOrdIDAndEstabCaseDateGreaterThanEqual(Integer planOrdID, Date estabCaseDate);
 
-	List<EstabCase> findByPlanOrdIDAndEstabCaseDateGreaterThanEqual(Integer planOrdID, Date estabCaseDate);
+    List<EstabCase> findByPlanOrdIDAndEstabCaseDateGreaterThanEqual(Integer planOrdID, Date estabCaseDate);
 
-	@Modifying
-	@Query("UPDATE EstabCase e SET e.estabCaseStatus = 3 WHERE e.planOrdID =:planOrdID AND e.estabCaseDate >=:estabCaseDate")
-	void updateEstabCaseStatus(@Param("planOrdID") Integer planOrdID, @Param("estabCaseDate") Date estabCaseDate);
+    @Modifying
+    @Query("UPDATE EstabCase e SET e.estabCaseStatus = 3 WHERE e.planOrdID =:planOrdID AND e.estabCaseDate >=:estabCaseDate")
+    void updateEstabCaseStatus(@Param("planOrdID") Integer planOrdID, @Param("estabCaseDate") Date estabCaseDate);
 
-	List<EstabCase> findByEmpIDAndEstabCaseStatusOrderByEstabCaseDateDesc(
-			Integer empID,
-			Integer estabCaseStatus
-	);
+    List<EstabCase> findByEmpIDAndEstabCaseStatusOrderByEstabCaseDateDesc(
+            Integer empID,
+            Integer estabCaseStatus
+    );
 
-	List<EstabCase> findByEmpID(Integer empID);
+    List<EstabCase> findByEmpID(Integer empID);
 
 	Page<EstabCase> findAllByTakeStatus(boolean takeStatus, Pageable pageable);
 }
