be0b8ff732b51e98dbc012c785f9825d344df545
YunHsuan Huang <mini50629@gmail.com>
2023-12-26 03:00:32 +0800

    修正product後台圖片上傳問題,加入可正常顯示資料的shopmall.html

--- src/main/java/com/furelise/shopcart/controller/ShopCartController.java
+++ src/main/java/com/furelise/shopcart/controller/ShopCartController.java
@@ -1,6 +1,9 @@
 package com.furelise.shopcart.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,58 +13,67 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.furelise.product.model.Product;
+import com.furelise.product.model.ProductService;
 import com.furelise.shopcart.model2.ShopCartService;
+import com.mysql.cj.x.protobuf.MysqlxCrud.Collection;
 
 @Controller
 @RequestMapping("/shopcart")
 public class ShopCartController {
 
-    @Autowired
-    private ShopCartService scSvc;
-    
-    @GetMapping("/")
-    public String viewCart(@RequestParam(name = "memID", required = false) Integer memID, Model model) {
-        if (memID == null) {
-        	String guest = "guestCart:guest";
-            // 如果 memID 為 null，將其視為訪客，執行相應的邏輯
-            model.addAttribute("cart", scSvc.getGuestCartProducts(guest));
-            Map<String, String> guestCart = scSvc.getGuestCartProducts(guest);
-            if(guestCart == null) {
-            	return "empty-cart";
-            }
-            return "shopcart";
-        } else {
-            // 如果 memID 不為 null，正常處理
-            model.addAttribute("cart", scSvc.getShopCartProducts(memID));
-            Map<String, String> memCart = scSvc.getShopCartProducts(memID);
-            if(memCart == null) {
-            	return "empty-cart";
-            }
-        }
-        return "shopcart";
-    }
+	@Autowired
+	private ShopCartService scSvc;
 
-    @PostMapping("/update")
-    public String updateCart(@RequestParam Integer memID, @RequestParam Integer pID, @RequestParam Integer quantity) {
-    	scSvc.updateQuantity(memID, pID, quantity);
-        return "redirect:/shopcart/view?memID=" + memID;
-    }
+	@Autowired
+	ProductService pSvc;
 
-    @PostMapping("/remove")
-    public String removeFromCart(@RequestParam Integer memID, @RequestParam Integer pID) {
-    	scSvc.removeProduct(memID, pID);
-        return "redirect:/shopcart/view?memID=" + memID;
-    }
+	@GetMapping("/")
+	public String viewCart(@RequestParam(name = "memID", required = false) Integer memID, Model model) {
 
-    @PostMapping("/clear")
-    public String clearCart(@RequestParam Integer memID) {
-    	scSvc.clearCart(memID);
-        return "redirect:/shopcart/view?memID=" + memID;
-    }
+		if (memID == null) {
+			String guest = "guestCart:guest";
+			Map<Product, String> guestCart = scSvc.getGuestCartProducts2(guest);
 
-//	//只是在頁面上修改數量
-//	@GetMapping 
-//	public void changeQuantity(Integer memID,Integer pID, Model model) {
-//		
-//	}
+			if (guestCart == null) {
+				return "empty-cart";
+			} else {
+				
+				Set<Map.Entry<Product,String>> cartEntrtSet = guestCart.entrySet();
+				model.addAttribute("cartEntrtSet",cartEntrtSet);
+			}
+			return "shopcart";
+		} else {
+			String mem = "mamCart:"+ memID;
+			Map<Product, String> memCart = scSvc.getGuestCartProducts2(mem);
+			
+			if (memCart == null) {
+				return "empty-cart";
+			} else {
+				Set<Map.Entry<Product,String>> cartEntrtSet = memCart.entrySet();
+				model.addAttribute("cartEntrtSet",cartEntrtSet);
+			}
+
+			return "shopcart";
+		}
+	}
+
+	@PostMapping("/update")
+	public String updateCart(@RequestParam Integer memID, @RequestParam Integer pID, @RequestParam Integer quantity) {
+		scSvc.updateQuantity(memID, pID, quantity);
+		return "redirect:/shopcart/view?memID=" + memID;
+	}
+
+	@PostMapping("/remove")
+	public String removeFromCart(@RequestParam Integer memID, @RequestParam Integer pID) {
+		scSvc.removeProduct(memID, pID);
+		return "redirect:/shopcart/view?memID=" + memID;
+	}
+
+	@PostMapping("/clear")
+	public String clearCart(@RequestParam Integer memID) {
+		scSvc.clearCart(memID);
+		return "redirect:/shopcart/view?memID=" + memID;
+	}
+
 }
