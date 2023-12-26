d40498a9b731b3205f9c544cd1072b46382834c4
Jackson <403490484@gms.tku.edu.tw>
2023-12-26 10:34:36 +0800

    finish picture upload

--- src/main/java/com/furelise/estabcase/controller/EmpOngoingCaseController.java
+++ src/main/java/com/furelise/estabcase/controller/EmpOngoingCaseController.java
@@ -1,5 +1,6 @@
 package com.furelise.estabcase.controller;
 
+import com.furelise.emp.model.EmpVO;
 import com.furelise.estabcase.empcasemanage.EmpOngoingCaseService;
 import com.furelise.estabcase.empcasemanage.EmpOngoingCaseVO;
 import com.furelise.estabcase.model.EstabCase;
@@ -9,12 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/estabcaseonging")
@@ -32,6 +37,7 @@ public class EmpOngoingCaseController {
         EmpOngoingCaseVO empOngoingCase = empOngoingCaseService.getEmpOngoingCase(estabCaseID);
 
         model.addAttribute("empOngoingCase",empOngoingCase);
+        model.addAttribute("estabCaseID",estabCaseID);
 
         return "jack_ongoingCases";
     }
@@ -51,18 +57,36 @@ public class EmpOngoingCaseController {
         return "redirect:/showImage";
     }
 
-//    @PostMapping("/add")
-//    public String statusPicSubmit(@Valid EstabCase estabCase, BindingResult result, ModelMap model) {
-//        if(result.hasErrors()) {
-//            return "b-product-add";
+//    @PostMapping("insert")
+//    public String insert(@Valid EstabCase estabCase, BindingResult result, ModelMap model,
+//                         @RequestParam("upFiles") MultipartFile[] parts) throws IOException {
+//
+//        /*************************** 1.接收請求參數 - 輸入格式的錯誤處理 ******************/
+//        // 去除BindingResult中upFiles欄位的FieldError紀錄
+//        List<FieldError> errorsListToKeep = result.getFieldErrors().stream()
+//                .filter(fieldname -> !fieldname.getField().equals("upFiles")).collect(Collectors.toList());
+//        result = new BeanPropertyBindingResult(estabCase, "estabCase");
+//        for (FieldError fieldError : errorsListToKeep) {
+//            result.addError(fieldError);
+//        }
+//        if (parts[0].isEmpty()) { // 使用者未選擇要上傳的圖片時
+//            model.addAttribute("errorMessage", "請上傳執行結果");
 //        } else {
-//            boolean isPass = empOngoingCaseService.addStstusPicture(estabCase);
-//            if(isPass) {
-//                return "redirect:/product-backen/all";
-//            } else {
-//                model.addAttribute("errorMsgs", "商品名稱重複");
-//                return "b-product-add";
+//            for (MultipartFile multipartFile : parts) {
+//                byte[] buf = multipartFile.getBytes();
+//                estabCase.setEstabCasePic(buf);
 //            }
 //        }
+//        if (result.hasErrors() || parts[0].isEmpty()) {
+//            return "jack_ongoingCases";
+//        }
+//        /*************************** 2.開始新增資料 ***************************************/
+////		EmpService empSvc = new EmpService();
+//        estabCaseRepository.save(estabCase);
+////        /*************************** 3.新增完成,準備轉交(Send the Success view) ***********/
+////        List<EmpVO> list = empSvc.getAll();
+////        model.addAttribute("empListData", list);
+////        model.addAttribute("success", "- (新增成功)");
+//        return "redirect:/jack_caseManagement"; // 新增成功後重導至IndexController_inSpringBoot.java的第50行@GetMapping("/emp/listAllEmp")
 //    }
 }
