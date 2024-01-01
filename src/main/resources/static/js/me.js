9dd8eaee5c83fd746429e8738bfe3798a0fb54f2
Rocky <rocky800402@gmail.com>
2024-01-01 20:45:18 +0800

    update:html更新

--- src/main/resources/static/js/me.js
+++ src/main/resources/static/js/me.js
@@ -9,11 +9,25 @@ const getMe = async () => {
     })
 }
 
+const getEmpMe = async () => {
+    return $.ajax({
+        url: `${API_PATH}/auth/emp/me`,
+        type: "GET",
+        xhrFields: {
+            withCredentials: true
+        },
+        dataType: "json"
+    })
+}
+
+
+
 $(document).ready(async () => {
     let mem
-
+    let emp
     try {
          mem = await getMe()
+         emp = await getEmpMe()
     } catch (error) {
         $("#logoutSection").attr("class", "hidden");
 //        if(!window.location.href.includes('login')){
@@ -22,6 +36,11 @@ $(document).ready(async () => {
     }
     if(mem){
         $(".mb_mem_name, .mb-mem-btn").text(mem.memName);
+        $("#loginSection").attr("class", "hidden");
+    }
+
+    if(emp){
+        $(".emp_mem_name").text(emp.empName);
         $("#loginSection").attr("class", "hidden");
     }
         
