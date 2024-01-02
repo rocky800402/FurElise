bb9337f843c2b569aa424eb0c2d9aa88a3e63c64
Sonya Lee <t1i5n2a8t@gmail.com>
2024-01-02 14:25:53 +0800

    add picture of plan

--- src/main/resources/static/js/planordshop.js
+++ src/main/resources/static/js/planordshop.js
@@ -110,7 +110,7 @@ function new_plan() {
                             <div class="col">
                                 <div class="p-3 border bg-light" style="text-align: center;">
                                     <p class="sl_plan_name">${data[i].planName}</p>
-                                    <img src="/images/product.png" alt="" width="90%">
+                                    <img src="/images/garbage.png" alt="" width="90%">
                                     <p>介紹還沒想</p>
                                 </div>
                             </div>
